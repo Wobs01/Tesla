559489e3e6f61d0dd87126569cbfc3100866e625
wobs01 <robin@rverhoeven.nl>
2019-11-05 21:26:16 +0100

    Authorization function

---
+++ Teslamodule.ps1
@@ -0,0 +1,56 @@
+function New-TeslaConnection {
+    <#   
+   .SYNOPSIS   
+   Function to connect to a SQL database
+       
+   .DESCRIPTION 
+   Opens a connection to the Tesla API or refreshes the token. Returns the password grant which can be used to follow up on other functions
+
+   .NOTES	
+       Author: Robin Verhoeven
+       Requestor: -
+       Created: -
+       
+       
+
+   .LINK
+       https://github.com/Wobs01/Tesla
+
+   .EXAMPLE   
+   . New-TeslaConnection -credentials $cred
+   
+
+   #>
+   
+   
+    [Cmdletbinding(DefaultParameterSetName = "NewToken")] 
+    param([parameter(Mandatory = $false,ParameterSetName='NewToken')]
+        [PSCredential]$credentials,
+        [parameter(Mandatory = $false,ParameterSetName='Refreshtoken')]
+        [string]$refresh_token,
+        [parameter(Mandatory = $false)]
+        [string]$URL = "https://owner-api.teslamotors.com/" 
+        
+                
+    ) 
+    
+    $loginhash = @{
+        "grant_type"= "password";
+        "client_id"= "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384";
+        "client_secret"= "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3";
+        "email"= $credentials.UserName;
+        "password"= $credentials.GetNetworkCredential().password;
+    }
+    $loginJSON = $loginhash | ConvertTo-Json
+
+    try {
+        $requestURI = $URL+"/oauth/token"
+        $response = Invoke-RestMethod -Method Post -Uri $requestURI -Body $loginJSON -ContentType "application/json"
+    }
+    catch {
+        throw $Error[0]
+    }
+    
+    return $response
+}
+
