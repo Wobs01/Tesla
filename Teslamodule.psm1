6b93b3d0973619764d58861acd5e3875a324b058
wobs01 <robin@rverhoeven.nl>
2019-11-07 20:27:40 +0100

    Added help in function

--- Teslamodule.psm1
+++ Teslamodule.psm1
@@ -38,26 +38,26 @@ function New-TeslaConnection {
             $credentials = Get-Credential -Message "Please provide your Tesla credentials" -ErrorAction Stop
         }
         catch {
-            throw $Error[0]
+            throw ("Unable to get credentials, please try again:`n"+$global:Error[0].ToString())
         }
     }
     
     switch ($PSCmdlet.ParameterSetName) {
         "Refreshtoken" {
             $loginhash = @{
+                "grant_type"    = "refresh_token";
+                "client_id"     = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384";
+                "client_secret" = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3";
+                "refresh_token" = $token.refresh_token
+            }
+        }
+        default {
+            $loginhash = @{
                 "grant_type"    = "password";
                 "client_id"     = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384";
                 "client_secret" = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3";
                 "email"         = $credentials.UserName;
                 "password"      = $credentials.GetNetworkCredential().password;
-            }
-        }
-        default {
-            $loginhash = @{
-                "grant_type"    = "refresh_token";
-                "client_id"     = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384";
-                "client_secret" = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3";
-                "refresh_token" = $token.refresh_token
             }
         }
     }
@@ -67,15 +67,38 @@ function New-TeslaConnection {
         $loginJSON = $loginhash | ConvertTo-Json
         $requestURI = $URL + "/oauth/token"
         $global:token = Invoke-RestMethod -Method Post -Uri $requestURI -Body $loginJSON -ContentType "application/json" -ErrorAction Stop
+        Write-Host "Authentication Successfull"
     }
     catch {
-        $Error[0].Exception | out-host
-        write-error $Error[0].Exception
+        ("Login Failed:`n"+$global:Error[0].ToString())
     }    
     
 }
 
 function Get-TeslaVehiclelist {
+    <#   
+   .SYNOPSIS   
+   Function to get vehicle list from the Tesla API
+       
+   .DESCRIPTION 
+   List the vehicle list for the logged in account, a vehicle id can be specified
+
+   .NOTES	
+       Author: Robin Verhoeven
+       Requestor: -
+       Created: -
+       
+       
+
+   .LINK
+       https://github.com/Wobs01/Tesla
+
+   .EXAMPLE   
+   . Get-TeslaVehiclelist -id <id>
+   
+
+   #>
+    
     [Cmdletbinding()] 
     param([parameter(Mandatory = $false)]
         [string]$id,
@@ -92,7 +115,7 @@ function Get-TeslaVehiclelist {
         $vehiclelist = Invoke-RestMethod -Method Get -Uri $requestURI -Headers $header -ContentType "application/json" -ErrorAction Stop
     }
     catch {
-        throw ("Unable to get vehicle, Error message:`n"+$global:Error[0].Exception | Out-String)
+        throw ("Unable to get vehicle, Error message:`n"+$global:Error[0].Exception.ToString())
        
     }
     return $vehiclelist.response 
